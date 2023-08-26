25b105bbbffc91da30a1be9a4343067dda00570b
Iryna <ira.leshtaieva@gmail.com>
2023-08-26 09:52:07 +0200

    adding hidden button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-import logo2 from './logo2.svg';
 import './App.css';
 import Header from './components/header/header';
 import Body from './components/body/body';
@@ -68,10 +67,7 @@ function App() {
   return (
 
     <div className='App'>
-      <header className="App-header">
-        <img src={logo2} className="App-logo" alt="logo" />
-        <Header/>
-      </header>
+      <Header/>
       <div className="App-content">
         <div className='App-menu'>
           <Body className='App-menu-row1' /> 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,31 @@
 import './header.css';
+import logo2 from './logo2.svg';
+
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [hidden, setHidden] = useState(false);
+    console.log(hidden);
+    function onClickHandler() {
+        setHidden(!hidden); 
+    }
+
     return(
-        <div className="header">
-            <h3>
-            Looking for a unique gift or want to decorate your interior?
-            Original watercolor magnets and Paintings will improve the mood of you and your friends and add zest to your home. 
-            Unique postcards will delight your friends and relatives</h3>
-        </div>
+        <header className="App-header">
+            <img src={logo2} className="App-logo" alt="logo" />
+            <div className="header">
+                <h3>
+                Looking for a unique gift or want to decorate your interior?
+                Original watercolor magnets and Paintings will improve the mood of you and your friends and add zest to your home. 
+                Unique postcards will delight your friends and relatives</h3>
+            </div>
+            <button className="hideHeader" 
+                onClick={onClickHandler}
+                >
+            </button>
+        </header>
+
     )
 }
-
 export default Header;
