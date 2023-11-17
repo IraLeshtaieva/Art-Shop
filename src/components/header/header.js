6cff584ca3fae6f861027249916029a5d18d048d
Iryna <ira.leshtaieva@gmail.com>
2023-11-17 21:52:00 +0100

    adding hidden icon implement hide button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,19 +13,24 @@ const Header = () => {
 
     return(
         <header className="App-header">
-            <img src={logo2} className="App-logo" alt="logo" />
-            <div className="header">
-                <h3>
-                Looking for a unique gift or want to decorate your interior?
-                Original watercolor magnets and Paintings will improve the mood of you and your friends and add zest to your home. 
-                Unique postcards will delight your friends and relatives</h3>
-            </div>
-            <button className="hideHeader" 
-                onClick={onClickHandler}
-                >
+            {!hidden? 
+            <> 
+                <img src={logo2} className="App-logo" alt="logo"/> 
+                <div className="header">
+                    <h3>
+                    Looking for a unique gift or want to decorate your interior?
+                    Original watercolor magnets and Paintings will improve the mood of you and your friends and add zest to your home. 
+                    Unique postcards will delight your friends and relatives</h3>
+                </div>
+            </>: null}
+            
+            
+            <button className="hideHeader">
+                <i className={'fa-solid fa-2x fa-circle-' + (hidden ? 'down': 'up')}
+                   onClick={onClickHandler}>
+                    </i>  
             </button>
         </header>
-
     )
 }
 export default Header;
